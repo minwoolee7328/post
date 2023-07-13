88b9c89d8f05843b89352de9883d9d5c1ae3c784
LEEMINWOO <mw7328@naver.com>
2023-07-14 04:31:36 +0900

    글로번 AOP 사용

---
+++ src/main/java/com/example/post/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.example.post.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice  // 모든 컨드롤러의 Exception 발생을 관리할 수 있다.
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<RestApiException> handleException(IllegalArgumentException ex) {
+        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(
+                // HTTP body
+                restApiException,
+                // HTTP status code
+                HttpStatus.BAD_REQUEST
+        );
+    }
+}
